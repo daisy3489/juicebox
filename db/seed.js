2fb0644eba01a5dbd546d8462b3398099f849c4f
daisy3489 <daisy.centeno13@gmail.com>
2021-09-09 21:22:22 -0700

    Move into node

---
+++ db/seed.js
@@ -0,0 +1,23 @@
+// inside db/seed.js
+
+// grab our client with destructuring from the export in index.js
+const { client } = require('./index');
+
+async function testDB() {
+  try {
+    // connect the client to the database, finally
+    client.connect();
+
+    // queries are promises, so we can await them
+    const { rows } = await client.query(`SELECT * FROM users;`);
+    console.log(rows);
+    
+  } catch (error) {
+    console.error(error);
+  } finally {
+    // it's important to close out the client connection
+    client.end();
+  }
+}
+
+testDB();
